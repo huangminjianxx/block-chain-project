0fb495b635b62bbbd94c28430593ed676e022041
黄敏坚 <>
2024-12-04 11:40:04 +0800

    修改next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: [  "@ant-design", "antd", "rc-util", "rc-pagination", "rc-picker" ]
+  transpilePackages: [ "antd", "@ant-design", "rc-util", "rc-pagination", "rc-picker", "rc-notification", "rc-tooltip", "rc-tree", "rc-table" ]
 }
 
 module.exports = nextConfig
